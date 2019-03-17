67c85dfc60b0ae84dfc1b9b4989c77249a76bfa9
wenying <2515957145@qq.com>
2019-03-17 17:03:10 +0800

    Given an array of integers, return indices of the two numbers such that they add up to a specific target.

---
+++ 1.Two Sum.py
@@ -0,0 +1,21 @@
+class Solution:
+    def twoSum(self, nums, target):
+        dicts = {}
+        for i in range(len(nums)):
+            x = nums[i]
+            if x in dicts:
+                return [dicts.get(x), i]
+            dicts[target-x] = i
+        print(dicts)
+
+    # def twoSum(self, nums, target):
+    #     for (index, x) in enumerate(nums):
+    #         y = target - x
+    #         # 判断y是否在x之后的列表里
+    #         if y in nums[index+1:]:
+    #             return [index, nums[index+1:].index(y)+index+1]
+
+
+if __name__ == "__main__":
+    s = Solution()
+    s.twoSum([2, 7, 11, 15], 9)
