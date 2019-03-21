1b1a2c76d3df0d1a60fd8e110434272bd4957a9f
CodingBuye <2515957145@qq.com>
2019-03-21 14:22:02 +0800

    完成第709题

---
+++ Easy/709.To Lower Case.py
@@ -0,0 +1,10 @@
+class Solution:
+    def toLowerCase(self, str: str) -> str:
+        """
+        :param str: 字符串
+        :return: 返回字符串的小写形式
+        """
+        return str.lower()
+
+
+print(Solution().toLowerCase("AcdcdAfCDCBDH"))
