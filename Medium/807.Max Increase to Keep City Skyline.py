b34e99ee2e1f5d3239de25a3d708e7ab9bd67a47
CodingBuye <2515957145@qq.com>
2019-06-19 00:04:38 +0800

    skyline增加的高度

--- Medium/807.Max Increase to Keep City Skyline.py
+++ Medium/807.Max Increase to Keep City Skyline.py
@@ -27,5 +27,6 @@ class Solution:
         return height_add
 
 
-grid = [[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 0]]
-print(Solution().maxIncreaseKeepingSkyline(grid))
+if __name__ == "__main__":
+    grid = [[3, 0, 8, 4], [2, 4, 5, 7], [9, 2, 6, 3], [0, 3, 1, 0]]
+    print(Solution().maxIncreaseKeepingSkyline(grid))
