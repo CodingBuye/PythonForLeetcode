b08684e4430773a1a3ca80c89977c71a4109952b
CodingBuye <2515957145@qq.com>
2019-06-23 22:37:53 +0800

    树的后序遍历

---
+++ Easy/590.N-ary Tree Postorder Traversal.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, x, children):
+        self.val = x
+        self.children = children
+
+
+class Solution:
+    def postorder(self, root):
+        res = []
+        if root:
+            if root.children:
+                for child in root.children:
+                    res += self.postorder(child)
+                res.append(root.val)
+            else:
+                res.append(root.val)
+        return res
